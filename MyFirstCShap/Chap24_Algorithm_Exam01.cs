9920eb12a8dc80542c701982c45a771dee730a51
준영 황 <roving324@naver.com>
2022-11-29 23:40:39 +0900

    MyFirstCShap 교육 내용 11/29

--- MyFirstCShap/Chap24_Algorithm_Exam01.cs
+++ MyFirstCShap/Chap24_Algorithm_Exam01.cs
@@ -13,38 +13,32 @@ namespace MyFirstCSharp
 
         private void btnMakeRandomAndSum_Click(object sender, EventArgs e)
         {
+            txtResult.Text= string.Empty;
             // 랜덤 난수 생성 및 없는 수 합치기.
+            Dictionary<int, int> Dic = new Dictionary<int, int>();
             Random ran = new Random();
             int[] iValues = new int[20];
             int iResults = 0;
+            string sValues = string.Empty;
+
             for (int i = 0; i < 20; i++)
             {
                 int iResult = ran.Next(0, 20);
                 txtResult.Text += Convert.ToString(iResult) + " ";
-                
-                iValues[i] = iResult;
+                if (Dic.ContainsKey(iResult)) continue;
+                else Dic.Add(iResult, 0);
             }
-            Array.Sort(iValues);
-            string sValues = "";
-            int iFind = -1;
+            
             for (int i = 0; i <= 20; i++)
             {
-                for (int j = 0; j < 20; j++)
+                if (!(Dic.ContainsKey(i)))
                 {
-                    if (i == iValues[j])
-                    {
-                        break;
-                    }
-                    if (iFind == i)
-                    {
-                        break;
-                    }
-                    iFind = i;
-                    sValues += i.ToString() + " ";
-                    iResults += i;
+                    Dic.Add(i, i);
+                    sValues += Dic[i] + " ";
+                    iResults += Dic[i];
                 }
             }
             MessageBox.Show($"난수 중 {sValues} 가 없으며 총 합은 {iResults} 입니다.");
-        } 
+        }
     }
 }
--- MyFirstCShap/Chap24_Algorithm_Exam02.cs
+++ MyFirstCShap/Chap24_Algorithm_Exam02.cs
@@ -15,6 +15,16 @@ namespace MyFirstCSharp
         {
             // 랜덤 난수 생성 클래스
             Random Ran = new Random();
+            int iPrice = Ran.Next(100, 3000);
+            int iCount = Ran.Next(1, 20);
+            int iMyPrice = Ran.Next(10000, 500000);
+            int iPriceCount = iPrice * iCount;
+            int iResultPrice = iMyPrice - iPriceCount * iCount;
+
+            if(iResultPrice < 0)
+            MessageBox.Show($"{iMyPrice} 원이 있을 때 이용요금 {iPrice} 원 인 놀이기구를 {iCount} 번 타면 {-iResultPrice} 원이 모자릅니다.");
+            else
+            MessageBox.Show($"{iMyPrice} 원이 있을 때 이용요금 {iPrice} 원 인 놀이기구를 {iCount} 번 타면 {iResultPrice} 원이 남습니다.");
         }
     }
 }
